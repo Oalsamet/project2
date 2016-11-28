11229101059010178ed3cb05fb53a3c3afe828d1
oalsamet <othmansamet@gmail.com>
2016-11-27 23:54:14 -0500

    Intial setup

---
+++ src/objectnclassespackage/House.java
@@ -0,0 +1,13 @@
+package objectnclassespackage;
+
+public class House {
+
+	public void Room(){
+		System.out.println("Kids are sleeping in the Bed Roomm");	
+	}
+	
+	
+	
+}
+
+
---
+++ src/objectnclassespackage/TestHouse.java
@@ -0,0 +1,11 @@
+package objectnclassespackage;
+
+public class TestHouse extends House {
+
+	public static void main(String[] args) {
+		//System.out.println("Kids are sleeping in the Bed Roomm");
+		TestHouse testHouse = new TestHouse();
+		testHouse.Room();
+	}
+
+}
